e475385cd7f524a4345097da70dcdbe47aa77ffd
Quackenstien <donaldhawkins422@gmail.com>
2020-05-17 13:31:07 -0700

    built seconday ajax call to display uv index

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var APIKey = "5e2cb7014d6f15c944e2f9749a9cfd25";
 
 // Here we run our AJAX call to the OpenWeatherMap API
-$("#search-button").on("click", function () {
+$("#search-button").on("click", function (e) {
+  e.preventDefault();
+
   var name = $("#search-input").val();
 
   // Here we are building the URL we need to query the database
@@ -29,14 +31,28 @@ $("#search-button").on("click", function () {
       // Convert the temp to fahrenheit
       var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
-      // // add temp content to html
+      // // add weather content to html
       $("#temp-display").text("Temperature: " + tempF.toFixed(1) + "°F");
       $("#wind-display").text("Wind Speed: " + response.wind.speed + "MPH");
       $("#hum-display").text("Humidity: " + response.main.humidity + "%");
 
-      // Log the data in the console as well
-      // console.log("Wind Speed: " + response.wind.speed);
-      // console.log("Humidity: " + response.main.humidity);
-      // console.log("Temperature (F): " + tempF);
+      //Getting current uv index
+      var lon = response.coord.lon;
+      var lat = response.coord.lat;
+      var uvIndex =
+        "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+        APIKey +
+        "&lat=" +
+        lat +
+        "&lon=" +
+        lon;
+
+      $.ajax({
+        url: uvIndex,
+        method: "GET",
+      }).then(function (result) {
+        console.log(result);
+        $("#uv-display").text("UV Index: " + result.value);
+      });
     });
 });
